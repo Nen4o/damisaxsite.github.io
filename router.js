ba4976e2ee579dec68fdac4a148bef9575dc911b
Minko Urchev <urchevminko@gmail.com>
2025-09-04 12:11:20 +0300

    add forced redirect to https

--- router.js
+++ router.js
@@ -3,6 +3,16 @@ const router = require('express').Router();
 const homeController = require('./src/controllers/homeController');
 const imageController = require('./src/controllers/imageController');
 
+
+// force redirect to https
+router.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect('https://' + req.headers.host + req.url);
+    }
+    next();
+});
+
+
 router.use(homeController);
 router.use(imageController);
 
