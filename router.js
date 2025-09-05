be7e53e3b68c41311cee4ce83bd08601f25d15e2
Minko Urchev <urchevminko@gmail.com>
2025-09-05 11:55:41 +0300

    add contacts

--- router.js
+++ router.js
@@ -5,12 +5,12 @@ const imageController = require('./src/controllers/imageController');
 
 
 // force redirect to https
-router.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect('https://' + req.headers.host + req.url);
-    }
-    next();
-});
+// router.use((req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect('https://' + req.headers.host + req.url);
+//     }
+//     next();
+// });
 
 
 router.use(homeController);
